70fa918f16a9b5501fb045315691c9c69f7376bb
Bennet <bennet@commun-it.net>
2021-01-26 16:29:21 +0100

    zweite HTML zwischenspeicherung

--- website/assets/scripts/modules/click.js
+++ website/assets/scripts/modules/click.js
@@ -1,15 +1,15 @@
 class Click {
 constructor() {
-    this.content = document.querySelector('.site-header__content');
-    this.events();
+    // this.content = document.querySelector('.site-header__content');
+    //this.events();
 }
 
 events() {
-    this.content.addEventListener('click', () => this.action());
+    //this.content.addEventListener('click', () => this.action());
 }
 
 action() {
-    this.content.style.backgroundColor = 'orange';
+   // this.content.style.backgroundColor = 'orange';
 }
 }
 
