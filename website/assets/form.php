c2f33a5ff5dea68297d5edab38e9040edfc46af2
Bennet <bennet@commun-it.net>
2021-02-08 17:59:17 +0100

    finished ersten  drei container desktop

--- website/assets/form.php
+++ website/assets/form.php
@@ -1,88 +1,52 @@
 <?php
 
-//check if form was sent
-if($_POST){
-
-	$to = 'some@email.com';
-	$subject = 'Testing HoneyPot';
-	$header = "From: $name <$name>";
-
-	$name = $_POST['büro'];
-	$name = $_POST['name'];
-	$email = $_POST['email'];
-	$email = $_POST['phone'];
-	$message = $_POST['message'];
-
-	//honey pot field
-	$honeypotName = $_POST['realname'];
-	$honeypotEmail = $_POST['realemail'];
-
-	//check if the honeypot field is filled out. If not, send a mail.
-	if( ! empty( $honeypot ) ){
-		return; //you may add code here to echo an error etc.
-	}else{
-		mail( $to, $subject, $message, $header );
-	}
-}
-
-?>
-
-<?php
-
-$empfaenger = "bennet@commun-it.net";
-
+$to = "bennet@commun-it.net";
 $name = trim( substr( @$_POST["name"], 0, 50));
 $email = trim( substr( @$_POST["email"],0, 50));
-$betreff = trim( substr( @$_POST["betreff"],0, 50));
-$nachricht = trim( substr( @$_POST["nachricht"], 0, 500));
-$button = trim( substr( @$_POST["button"],0, 20));
+$phone = trim( substr( @$_POST["phone"],0, 50));
+$message = trim( substr( @$_POST["message"], 0, 500));
+$button = trim( substr( @$_POST["submit"],0, 20));
+$honeypotName = $_POST['realname'];
+$honeypotEmail = $_POST['realemail'];
+$meldung = [];
 
-
-$meldung =[];
 
 if($button){
     if( empty($name) ){
         $meldung[] = "Bitte Name angeben";
-    }
-    if( empty($betreff) ){
-        $meldung[] = "Bitte Betreff angeben";
+    } else {
+        echo htmlspecialchars($name)
     }
     if( empty($email) ){
         $meldung[] = "Bitte E-Mail angeben";
+    } else {
+        echo htmlspecialchars($email)
     }
-    if( empty($nachricht) ){
+    if( empty($phone) ){
+        $meldung[] = "Bitte telefon angeben";
+    } else {
+        echo htmlspecialchars($phone)
+    }
+    if( empty($message) ){
         $meldung[] = "Bitte Nachricht angeben";
+    } else {
+        echo htmlspecialchars($message)
     }
-    #Email-Adresse nicht korrekt?
-    elseif(filter_var($email, FILTER_VALIDATE_EMAIL) == false ){
-        $meldung[]="Bitte geben Sie eine gÃ¼ltige Email-Adresse ein";
+    if( !empty($honeypotName) || !empty($honeypotEmail)){
+		$meldung[] = "Fehler";
+	}
+    elseif( filter_var($email, FILTER_VALIDATE_EMAIL) == false ){
+        $meldung[]="Bitte geben Sie eine gültige Email-Adresse ein";
     }
 
-    ########Formulardaten verarbeiten########
-    #gibt es keine Meldung, dann Mail versenden
-    #count(array) zählt die werte eines Array 
-    #gab es keine User-Meldung, dass ist der Array leer
+    if(count($meldung) == 0) {
+        mail($to, $name, $phone, $message, "From: $name <$email>");
     
-if(count($meldung) == 0) {
-    ##Formulardaten als Mail versenden
-    #mail(EmpfÃ¤nger, Betreff, Nachricht, Absender)
- mail($empfaenger, $betreff, $nachricht, "From: $name <$email>");
-    
-    #Antwortseite anfordern
-    #im HTTP-Response wird eine Zeile untergebracht, die den Browser veranlasst, eine neue Seite anzufordern
-header("Location: index.php");
-}
-}
-#html fehler text
-if($meldung){
-    echo "<p style=\"color:red\">".implode("<br>",$meldung)."</p>";
+        header("Location: http://bennet.helix-marketing.de/test/");
+        
     }
+}
+include ('index.php');
 
 ?>
 
-<?php 
-    if ($meldung) { ?>
-     <p style="color:red;"><?php echo implode("<br>", $meldung) ?></p>
-<?php}?> 
-
-   
