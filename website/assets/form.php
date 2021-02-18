c3a3c17e77b2aa57727b02a4734aa81a00524ef4
Bennet <bennet@commun-it.net>
2021-02-18 18:18:06 +0100

    fixed bugs und started kontakt form

--- website/assets/form.php
+++ website/assets/form.php
@@ -1,11 +1,11 @@
 <?php
 
 $to = "bennet@commun-it.net";
-$name = trim( substr( @$_POST["name"], 0, 50));
-$email = trim( substr( @$_POST["email"],0, 50));
-$phone = trim( substr( @$_POST["phone"],0, 50));
-$message = trim( substr( @$_POST["message"], 0, 500));
-$button = trim( substr( @$_POST["submit"],0, 20));
+$name = trim( substr($_POST["name"], 0, 50));
+$email = trim( substr($_POST["email"],0, 50));
+$phone = trim( substr($_POST["phone"],0, 50));
+$message = trim( substr($_POST["message"], 0, 500));
+$button = trim( substr($_POST["submit"],0, 20));
 $honeypotName = $_POST['realname'];
 $honeypotEmail = $_POST['realemail'];
 $meldung = [];
@@ -17,21 +17,25 @@ if($button){
     } else {
         echo htmlspecialchars($name)
     }
+
     if( empty($email) ){
         $meldung[] = "Bitte E-Mail angeben";
     } else {
         echo htmlspecialchars($email)
     }
+
     if( empty($phone) ){
         $meldung[] = "Bitte telefon angeben";
     } else {
         echo htmlspecialchars($phone)
     }
+    
     if( empty($message) ){
         $meldung[] = "Bitte Nachricht angeben";
     } else {
         echo htmlspecialchars($message)
     }
+
     if( !empty($honeypotName) || !empty($honeypotEmail)){
 		$meldung[] = "Fehler";
 	}
@@ -42,7 +46,7 @@ if($button){
     if(count($meldung) == 0) {
         mail($to, $name, $phone, $message, "From: $name <$email>");
     
-        header("Location: http://bennet.helix-marketing.de/test/");
+        header("Location: http://bennet.helix-marketing.de/urbanke/");
         
     }
 }
