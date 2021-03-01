2314c62e12f42c084f7abc4e5c1d24ff0ee3f6aa
Bennet <bennet@commun-it.net>
2021-03-01 19:57:51 +0100

    end version 0.1

--- webpack.config.js
+++ webpack.config.js
@@ -1,100 +1,124 @@
-const currentTask = process.env.npm_lifecycle_event
-const path = require('path')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const fse = require('fs-extra')
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
-
+const currentTask = process.env.npm_lifecycle_event;
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const fse = require("fs-extra");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 //cssnano plugin für minfied
 //fs-extra für mehrere html files und copied folder
 //babel/core babel/preset-env babel-loader für leute mit älteren brwoser nur js
 
-const postCSSPlugins = [require("postcss-import"), 
-                        require("postcss-mixins"), 
-                        require("postcss-simple-vars"), 
-                        require("postcss-nested"), 
-                        require("postcss-hexrgba"), 
-                        require("postcss-color-function"), 
-                        require("autoprefixer")
-                    ]
+const postCSSPlugins = [
+  require("postcss-import"),
+  require("postcss-mixins"),
+  require("postcss-simple-vars"),
+  require("postcss-nested"),
+  require("postcss-hexrgba"),
+  require("postcss-color-function"),
+  require("autoprefixer"),
+];
 
 class RunAfterCompile {
-    apply(compiler) {
-        compiler.hooks.done.tap('Copy something', function() {
-            fse.copySync('./website/assets/images', './dist/assets/images'),
-            fse.copySync('./website/assets/fonts', './dist/assets/fonts')
-        });
-    }
+  apply(compiler) {
+    compiler.hooks.done.tap("Copy something", function () {
+      fse.copySync("./website/assets/images", "./dist/assets/images"),
+        fse.copySync("./website/assets/fonts", "./dist/assets/fonts");
+    });
+  }
 }
 
 let cssConfig = {
-    test:/\.css$/i,
-    use: ['css-loader', {loader:"postcss-loader", options: {postcssOptions: {plugins: postCSSPlugins}}}]
-}
+  test: /\.css$/i,
+  use: [
+    "css-loader",
+    {
+      loader: "postcss-loader",
+      options: { postcssOptions: { plugins: postCSSPlugins } },
+    },
+  ],
+};
 
 let config = {
-    entry: "./website/assets/scripts/app.js",
-    plugins: [
-        new HtmlWebpackPlugin({filename: 'index.html', template: './website/index.html'}),
-      ],
-    module: {
-        rules: [
-            cssConfig,
-            { 
-                test: /\.(jpg|webp|svg|woff|woff2|eot|ttf)$/, 
-                use: [{
-                    loader: 'url-loader',
-                    options: {
-                        limit: 10000,
-                        name: 'images/[name].[ext]'
-                        }
-                    }
-                ]
-            }
-        ]
-    }
+  entry: "./website/assets/scripts/app.js",
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "./website/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      cssConfig,
+      {
+        test: /\.(jpg|webp|svg)$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 10000,
+              name: "images/[name].[ext]",
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf)$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 10000,
+              name: "fonts/[name].[ext]",
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
+
+if (currentTask == "dev") {
+  cssConfig.use.unshift("style-loader");
+  config.output = {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "website"),
+  };
+  config.devServer = {
+    before: function (app, server) {
+      server._watch("./website/**/*.html");
+    },
+    contentBase: path.join(__dirname, "website"),
+    port: 1234,
+    hot: true,
+    host: "0.0.0.0",
+  };
+  config.mode = "development";
 }
 
-if(currentTask == 'dev') {
-    cssConfig.use.unshift('style-loader')
-    config.output = {
-        filename: "bundle.js",
-        path: path.resolve(__dirname, 'website')
-    }
-    config.devServer = {
-        before: function(app, server) {
-            server._watch('./website/**/*.html')
-        },
-        contentBase: path.join(__dirname, 'website'),
-        port: 1234,
-        hot: true,
-        host: '0.0.0.0'
-    }
-    config.mode = "development"
+if (currentTask == "build") {
+  cssConfig.use.unshift(MiniCssExtractPlugin.loader);
+  config.output = {
+    filename: "[name].[chunkhash].js",
+    chunkFilename: "[name].[chunkhash].js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "",
+  };
+  config.mode = "production";
+  config.optimization = {
+    splitChunks: { chunks: "all" },
+    minimize: true,
+    minimizer: [`...`, new CssMinimizerPlugin({ parallel: true })],
+  };
+  config.plugins.push(
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "styles.[chunkhash].css",
+      linkType: "text/css",
+    }),
+    new RunAfterCompile()
+  );
 }
 
-if(currentTask == 'build') {
-    cssConfig.use.unshift(MiniCssExtractPlugin.loader)
-    config.output = {
-        filename: '[name].[chunkhash].js',
-        chunkFilename: '[name].[chunkhash].js',
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: ''
-    }
-    config.mode = "production"
-    config.optimization = {
-        splitChunks: {chunks: 'all'},
-        minimize: true,
-        minimizer: [`...`, new CssMinimizerPlugin({parallel: true})]
-    }
-    config.plugins.push(
-        new CleanWebpackPlugin(), 
-        new MiniCssExtractPlugin({filename:'styles.[chunkhash].css'}),
-        new RunAfterCompile()
-        )  
-}
-
-module.exports = config
-
+module.exports = config;
--- website/assets/scripts/modules/click.js
+++ website/assets/scripts/modules/click.js
@@ -1,26 +1,26 @@
 class Click {
-constructor() {
-    this.formId = document.querySelector('#formular');
-    this.formBtn = document.querySelector('#form-btn');
-    this.menuBtn = document.querySelector('#menu-btn');
-    this.menuItems = document.querySelectorAll('.site-header__menu-item');
+  constructor() {
+    this.formId = document.querySelector("#formular");
+    this.formBtn = document.querySelector("#form-btn");
+    this.menuBtn = document.querySelector("#menu-btn");
+    this.menuItems = document.querySelectorAll(".site-header__menu-item");
     this.menuUncheck();
     this.formSubmit();
+  }
+
+  menuUncheck() {
+    this.menuItems.forEach((menuItem) => {
+      menuItem.addEventListener("click", () => {
+        this.menuBtn.checked = false;
+      });
+    });
+  }
+
+  formSubmit() {
+    this.formBtn.addEventListener("click", (e) => {
+      e.preventDefault(this.formId.submit());
+    });
+  }
 }
 
-menuUncheck() {
-    this.menuItems.forEach(menuItem => {
-        menuItem.addEventListener('click', () => {
-            this.menuBtn.checked = false;
-    });
-    });
-}
-
-formSubmit() {
-    this.formBtn.addEventListener('click', () => {
-        this.formId.submit();
-    });
-}
-}
-
-export default Click
+export default Click;
