437e0874b4ab3a48566d1654289b95555c13e893
Bennet <bennet@commun-it.net>
2021-03-02 15:09:56 +0100

    letzte Verbesserungen

--- website/assets/scripts/modules/click.js
+++ website/assets/scripts/modules/click.js
@@ -5,7 +5,7 @@ class Click {
     this.menuBtn = document.querySelector("#menu-btn");
     this.menuItems = document.querySelectorAll(".site-header__menu-item");
     this.menuUncheck();
-    this.formSubmit();
+    // this.formSubmit();
   }
 
   menuUncheck() {
@@ -16,11 +16,11 @@ class Click {
     });
   }
 
-  formSubmit() {
-    this.formBtn.addEventListener("click", (e) => {
-      e.preventDefault(this.formId.submit());
-    });
-  }
+  // formSubmit() {
+  //   this.formBtn.addEventListener("click", (e) => {
+  //     e.preventDefault(this.formId.submit());
+  //   });
+  // }
 }
 
 export default Click;
